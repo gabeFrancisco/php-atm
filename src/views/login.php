9cf5a35ecdea540ea8d5b3f7d4b4fa5668daa15f
gabefrancisco <gabegit2@gmail.com>
2026-04-07 21:44:56 -0300

    estilizado pagina de login

--- src/views/login.php
+++ src/views/login.php
@@ -5,11 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="output.css">
-    <title>PHP_ATM</title>
+    <title>PHP_ATM - Login</title>
 </head>
 
-<body class="bg-blue-500">
-    <?php echo $nome; ?>
+<body class="bg-gradient-to-br from-blue-700 to-blue-400 flex flex-col justify-center items-center w-screen h-screen">
+    <div class="-mt-52 p-5 shadow-lg bg-white  rounded flex flex-col justify-center items-center">
+        <h3 class="text-indigo-700">PHP-ATM</h3>
+        <div>
+            <p class="text-sm mt-2">Prossiga com o login abaixo:</p>
+            <form action="POST" class="pt-5 my-3 border-t border-slate-200">
+                <div class="flex flex-row justify-between mb-5">
+                    <label for="conta">Nº Conta</label>
+                    <input type="text" id="conta" name="conta" class="rounded border p-0.5 ml-2 border-slate-400">
+                </div>
+                <div class="flex flex-row justify-between">
+                    <label for="senha">Senha</label>
+                    <input type="password" id="senha" name="senha" class="rounded border p-0.5 ml-2 border-slate-400">
+                </div>
+                <button
+                    class="w-full mt-5 bg-blue-600 text-white cursor-pointer hover:bg-blue-700 rounded py-2">Acessar!</button>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
