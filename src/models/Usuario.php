cab1f4121895639f0b6b22fd332044234ee9315e
gabefrancisco <gabegit2@gmail.com>
2026-04-08 08:08:12 -0300

    criado classe de modelo Usuario

---
+++ src/models/Usuario.php
@@ -0,0 +1,19 @@
+<?php
+
+use PDO;
+
+class Usuario
+{
+    public function __construct(public readonly PDO $db)
+    {
+    }
+
+    public function find(int $id)
+    {
+        $stmt = $this->db->prepare('SELECT * FROM usuarios WHERE id = ?');
+        $stmt->execute([$id]);
+
+        //se não achar, ele retorna null
+        return $stmt->fetch() ?: null;
+    }
+}
