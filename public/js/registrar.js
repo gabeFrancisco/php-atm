d7471fd138a88e12accb2b9b60eb74bc44fbb577
gabefrancisco <gabegit2@gmail.com>
2026-04-08 22:07:22 -0300

    implementado validações nos campos da pagina de registro

--- public/js/registrar.js
+++ public/js/registrar.js
@@ -1,11 +1,45 @@
 const conta_view = document.getElementById("conta_view");
 const endereco_view = document.getElementById("endereco_view");
+const aviso = document.getElementById('aviso')
 
-const nextButton = document.getElementById("next");
+const next_button = document.getElementById("next");
+const enviar = document.getElementById("enviar")
+const conta_form = document.getElementById('conta_form');
+const endereco_form = document.getElementById('endereco_form');
 
 endereco_view.style.display = 'none'
 
-nextButton.addEventListener('click', function () {
+next_button.addEventListener('click', function () {
+
+    if (!conta_form.checkValidity()) {
+        conta_form.reportValidity();
+        aviso.style.display = 'block';
+        return;
+    }
+
+    aviso.style.display = 'none'
     conta_view.style.display = 'none'
     endereco_view.style.display = 'block'
 })
+
+enviar.addEventListener('click', function () {
+
+    if (!endereco_form.checkValidity()) {
+        endereco_form.reportValidity();
+        aviso.style.display = 'block';
+        return;
+    }
+
+    aviso.style.display = 'none'
+
+    const conta_data = new FormData(conta_form);
+    const endereco_data = new FormData(endereco_form);
+
+    const form_final = new FormData();
+
+    for (const [key, value] of conta_data) form_final.append(key, value);
+    for (const [key, value] of endereco_data) form_final.append(key, value);
+
+
+})
+
--- src/views/registrar.php
+++ src/views/registrar.php
@@ -12,52 +12,66 @@
     class="bg-linear-to-br bg-no-repeat from-blue-700 to-blue-400 flex flex-col justify-center items-center w-screen h-screen">
     <div class="p-5 shadow-xl bg-white  rounded flex flex-col justify-center items-center">
         <img class="w-48 ml-5 mb-3" src="assets/logo.png" alt="Logo" />
-        <div id="conta_view">
-            <p class="text-sm mt-2">Preencha os dados abaixo para realizar o cadastro</p>
-            <div class="pt-5 my-3 border-t border-slate-200">
-                <div class="flex flex-row justify-between mb-3">
-                    <label for="nome">Nome</label>
-                    <input type="text" id="nome" name="nome" class="rounded border p-0.5 ml-2 border-slate-400">
+        <p class="text-sm my-2">*Obrigatório</p>
+        <p style="display: none" id="aviso"
+            class="text-sm text-red-500 text-center  p-2 rounded border border-red-500 w-full">Por
+            favor, revise
+            os campos!
+        </p>
+        <form id="conta_form">
+            <div id="conta_view">
+                <p class="text-sm mt-2">Preencha os dados abaixo para realizar o cadastro</p>
+                <div class="pt-5 my-3 border-t border-slate-200">
+                    <div class="flex flex-row justify-between mb-3">
+                        <label for="nome">Nome*</label>
+                        <input required required type="text" id="nome" name="nome"
+                            class="rounded border p-0.5 ml-2 border-slate-400">
+                    </div>
+                    <div class="flex flex-row justify-between mb-3">
+                        <label for="sobrenome">Sobrenome*</label>
+                        <input required type="text" id="sobrenome" name="sobrenome"
+                            class="rounded border p-0.5 ml-2 border-slate-400">
+                    </div>
+                    <div class="flex flex-row justify-between mb-3">
+                        <label for="email">Email*</label>
+                        <input required type="email" id="email" name="email"
+                            class="rounded border p-0.5 ml-2 border-slate-400">
+                    </div>
+                    <div class="flex flex-row justify-between mb-3">
+                        <label for="tipoConta">Tipo de Conta*</label>
+                        <select type="tipoConta" id="tipoConta" name="tipoConta"
+                            class="rounded border p-0.5 ml-2 border-slate-400">
+                            <option value="1">Corrente</option>
+                            <option value="2">Poupança</option>
+                        </select>
+                    </div>
+                    <div class="flex flex-row justify-between mb-3">
+                        <label for="conta">CPF*</label>
+                        <input required type="text" id="conta" name="conta"
+                            class="rounded border p-0.5 ml-2 border-slate-400">
+                    </div>
+                    <div class="flex flex-row justify-between">
+                        <label for="senha">Senha*</label>
+                        <input required type="password" id="senha" name="senha"
+                            class="rounded border p-0.5 ml-2 border-slate-400">
+                    </div>
+                    <button type="button" id="next" class="w-full mt-5 btn-primary cursor-pointer rounded py-2">Próximo
+                        -></button>
                 </div>
-                <div class="flex flex-row justify-between mb-3">
-                    <label for="sobrenome">Sobrenome</label>
-                    <input type="text" id="sobrenome" name="sobrenome"
-                        class="rounded border p-0.5 ml-2 border-slate-400">
-                </div>
-                <div class="flex flex-row justify-between mb-3">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" class="rounded border p-0.5 ml-2 border-slate-400">
-                </div>
-                <div class="flex flex-row justify-between mb-3">
-                    <label for="tipoConta">Tipo de Conta</label>
-                    <select type="tipoConta" id="tipoConta" name="tipoConta"
-                        class="rounded border p-0.5 ml-2 border-slate-400">
-                        <option value="1">Corrente</option>
-                        <option value="2">Poupança</option>
-                    </select>
-                </div>
-                <div class="flex flex-row justify-between mb-3">
-                    <label for="conta">CPF</label>
-                    <input type="text" id="conta" name="conta" class="rounded border p-0.5 ml-2 border-slate-400">
-                </div>
-                <div class="flex flex-row justify-between">
-                    <label for="senha">Senha</label>
-                    <input type="password" id="senha" name="senha" class="rounded border p-0.5 ml-2 border-slate-400">
-                </div>
-                <button type="button" id="next" class="w-full mt-5 btn-primary cursor-pointer rounded py-2">Próximo
-                    -></button>
-            </div>
-        </div>
+        </form>
+    </div>
+    <form id="endereco_form">
         <div id="endereco_view">
             <p class="text-sm mt-2">Preencha os dados abaixo para realizar o cadastro</p>
             <div class="pt-5 my-3 border-t border-slate-200">
                 <div class="flex flex-row justify-between mb-3">
                     <label for="rua">Rua</label>
-                    <input type="text" id="rua" name="rua" class="rounded border p-0.5 ml-2 border-slate-400">
+                    <input required type="text" id="rua" name="rua" class="rounded border p-0.5 ml-2 border-slate-400">
                 </div>
                 <div class="flex flex-row justify-between mb-3">
                     <label for="numero">Número</label>
-                    <input type="text" id="numero" name="numero" class="rounded border p-0.5 ml-2 border-slate-400">
+                    <input required type="text" id="numero" name="numero"
+                        class="rounded border p-0.5 ml-2 border-slate-400">
                 </div>
                 <div class="flex flex-row justify-between mb-3">
                     <label for="complemento">Complemento</label>
@@ -65,21 +79,23 @@
                         class="rounded border p-0.5 ml-2 border-slate-400">
                 </div>
                 <div class="flex flex-row justify-between mb-3">
-                    <label for="bairro">Bairro</label>
+                    <label required for="bairro">Bairro</label>
                     <input type="text" id="bairro" name="bairro" class="rounded border p-0.5 ml-2 border-slate-400">
                 </div>
                 <div class="flex flex-row justify-between mb-3">
-                    <label for="cidade">Cidade</label>
+                    <label required for="cidade">Cidade</label>
                     <input type="text" id="cidade" name="cidade" class="rounded border p-0.5 ml-2 border-slate-400">
                 </div>
                 <div class="flex flex-row justify-between mb-3">
-                    <label for="estado">Estado</label>
+                    <label required for="estado">Estado</label>
                     <input type="text" id="estado" name="estado" class="rounded border p-0.5 ml-2 border-slate-400">
                 </div>
-                <button class="w-full mt-5 btn-primary cursor-pointer rounded py-2">Enviar!</button>
+                <button id="enviar" class="w-full mt-5 btn-primary cursor-pointer rounded py-2">Enviar!</button>
             </div>
         </div>
+    </form>
     </div>
+
     <script src="js/registrar.js"></script>
 </body>
 
