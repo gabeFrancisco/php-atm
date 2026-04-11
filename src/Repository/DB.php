49ae660fef9c1a3ccc5dc6e9c4f418e41dd076e8
gabefrancisco <gabegit2@gmail.com>
2026-04-11 19:53:34 -0300

    criado classe DB para gerenciar chamadas ao PDO

---
+++ src/Repository/DB.php
@@ -0,0 +1,31 @@
+<?php
+
+class DB
+{
+    private static $host = '127.0.0.1';
+    private static $port = '5432';
+    private static $dbname = 'php_atm';
+    private static $user = 'postgres';
+    private static $password = '1234';
+
+    private static $instancia;
+
+    public static function connect()
+    {
+        if (!isset(self::$instancia)) {
+            try {
+                $dsn = "pgsql:host=" . self::$host . ";port=" . self::$port . ";dbname=" . self::$dbname;
+
+                self::$instancia = new PDO($dsn, self::$user, self::$password, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false
+                ]);
+            } catch (PDOException $e) {
+                die("ERRO DE CONEXÂO: " . $e->getMessage());
+            }
+        }
+
+        return self::$instancia;
+    }
+}
